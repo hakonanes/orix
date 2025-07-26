250b7c1bf0a5e9411851241cfe1ed99e9edcb55b
Håkon Wiik Ånes <hwaanes@gmail.com>
2025-07-26 15:31:52 +0200

    Add new function for directly registering custom Matplotlib projections (#579)
    Signed-off-by: Håkon Wiik Ånes <hwaanes@gmail.com>

--- orix/plot/crystal_map_plot.py
+++ orix/plot/crystal_map_plot.py
@@ -1,4 +1,5 @@
-# Copyright 2018-2024 the orix developers
+#
+# Copyright 2018-2025 the orix developers
 #
 # This file is part of orix.
 #
@@ -9,11 +10,12 @@
 #
 # orix is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with orix.  If not, see <http://www.gnu.org/licenses/>.
+# along with orix. If not, see <http://www.gnu.org/licenses/>.
+#
 
 from typing import List, Optional
 
@@ -22,7 +24,6 @@ import matplotlib.colorbar as mbar
 from matplotlib.image import AxesImage
 import matplotlib.patches as mpatches
 from matplotlib.projections import register_projection
-import matplotlib.pyplot as plt
 from matplotlib_scalebar.dimension import _Dimension
 from matplotlib_scalebar.scalebar import ScaleBar
 from mpl_toolkits.axes_grid1 import make_axes_locatable
